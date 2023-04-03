a708acee586e2e5e1aab38af6a856025bad4b9db
George Paul <43181118+GeorgeAPaul@users.noreply.github.com>
2023-04-03 12:21:33 +0200

    Update astar.py

--- src/neural_astar/planner/astar.py
+++ src/neural_astar/planner/astar.py
@@ -199,15 +199,18 @@ class NeuralAstar(VanillaAstar):
             AstarOutput: search histories and solution paths, and optionally intermediate search results.
         """
 
-        encoded_cost = self.encode(map_designs/torch.max(map_designs), start_maps, goal_maps)
-        #print("Encoded cost min" + str(torch.min(encoded_cost)))
-        print("Encoded cost max" + str(torch.max(encoded_cost)))
+        normalising_factor = torch.max(map_designs)
+        normalised_maps = map_designs/normalising_factor
 
-        encoded_sum = map_designs + encoded_cost * torch.max(map_designs)
+        encoded_cost = self.encode(normalised_maps, start_maps, goal_maps)
+        #print("Encoded cost min" + str(torch.min(encoded_cost)))
+        #print("Encoded cost max" + str(torch.max(encoded_cost)))
+
+        encoded_sum = normalised_maps + encoded_cost
         #print("Encoded sum min" + str(torch.min(encoded_sum)))
         #print("Encoded sum max" + str(torch.max(encoded_sum)))
 
-        cost_maps = (encoded_sum/torch.max(encoded_sum)) * torch.max(map_designs)
+        cost_maps = encoded_sum#(encoded_sum/torch.max(encoded_sum)) * torch.max(map_designs)
         #print("Cost maps min" +str(torch.min(cost_maps)))
         #print("Cost maps max" +str(torch.max(cost_maps)))
         obstacles_maps = (
